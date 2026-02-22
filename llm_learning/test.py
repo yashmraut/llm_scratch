9e53a1c792fa9d74edf5f8e8c4cee47792e8d3b2
yashmraut <yash77raut@gmail.com>
2026-02-22 09:23:26 +0530

    folder restructured

---
+++ llm_learning/test.py
@@ -0,0 +1,22 @@
+
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="llama-3.3-70b-versatile",
+)
+
+print(chat_completion.choices[0].message.content)
